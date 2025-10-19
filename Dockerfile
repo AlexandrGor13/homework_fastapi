85a795508eb7b25969e7f4ccfb212ed06946f991
alexandr <gorbenich@gmail.com>
2025-10-19 23:31:17 +0300

    create app

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.13
+LABEL authors="alexandr"
+
+COPY pyproject.toml uv.lock app/
+
+WORKDIR app
+
+RUN pip3 install --upgrade pip && pip3 install uv
+RUN uv sync
+
+COPY ./ ./
+
+ENV PATH="/app/.venv/bin:$PATH"
---
+++ ml.py
@@ -0,0 +1,28 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+from schemas import Text
+
+langlist = ["ru", "en"]
+tokenizer = {}
+model = {}
+for i in range(len(langlist)):
+    key = f"{langlist[i % 2]}-{langlist[(i + 1) % 2]}"
+    tokenizer[key] = AutoTokenizer.from_pretrained(f"Helsinki-NLP/opus-mt-{key}")
+    model[key] = AutoModelForSeq2SeqLM.from_pretrained(f"Helsinki-NLP/opus-mt-{key}")
+
+
+def translate(original_text: Text, src: str, dest: str):
+    current_model = model.get(f"{src}-{dest}")
+    current_tokenizer = tokenizer.get(f"{src}-{dest}")
+    translated_text = ""
+    if current_model:
+        # подготовка входных данных
+        tokenized_text = current_tokenizer([original_text.text], return_tensors='pt')
+        # перевод
+        translation = current_model.generate(**tokenized_text)
+        translated_text = current_tokenizer.batch_decode(translation, skip_special_tokens=True)[0]
+    return Text(text=translated_text)
+
+
+if __name__ == "__main__":
+    assert translate(Text(text="hello world"), src="en", dest="ru").text == "Приветствую мир", "Error"
